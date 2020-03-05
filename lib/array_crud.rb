ae5454e4d5d82bedcf9c168b2c5a72e58483b3f1
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-05 19:31:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["queso", "guac", "pico", "sour cream"]
+  qdoba = ["queso", "guac", "pico", "sour cream"]
 end
 
 def add_element_to_end_of_array(array, element)
